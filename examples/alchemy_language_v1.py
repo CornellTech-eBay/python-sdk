606e389e60b26ddeae5c72acedc441427ccf70b0
ichigooo <velvetland626@gmail.com>
2016-11-21 14:21:37 -0500

    delete redundant files, run ebay keywords

--- examples/alchemy_language_v1.py
+++ examples/alchemy_language_v1.py
@@ -2,21 +2,19 @@ import json
 from os.path import join, dirname
 from watson_developer_cloud import AlchemyLanguageV1
 
-alchemy_language = AlchemyLanguageV1(api_key='YOUR API KEY')
+alchemy_language = AlchemyLanguageV1(api_key='df458bfdb22d816f67f56cf375d0a7b0b1b2c172')
 
 url = 'https://developer.ibm.com/watson/blog/2015/11/03/price-reduction-for-watson-personality-insights/'
-
-print(json.dumps(alchemy_language.targeted_sentiment(text='I love cats! Dogs are smelly.',
-                                                     targets=['cats', 'dogs'], language='english'), indent=2))
-# print(json.dumps(alchemy_language.targeted_emotion(text='I love apples. I hate bananas',
-#                                                    targets=['apples', 'bananas'], language='english'), indent=2))
 
 # print(json.dumps(alchemy_language.author(url=url), indent=2))
 # print(json.dumps(alchemy_language.concepts(max_items=2, url=url), indent=2))
 # print(json.dumps(alchemy_language.dates(url=url, anchor_date='2016-03-22 00:00:00'), indent=2))
 # print(json.dumps(alchemy_language.emotion(url=url), indent=2))
-# print(json.dumps(alchemy_language.entities(url=url), indent=2))
-# print(json.dumps(alchemy_language.keywords(max_items=5, url=url), indent=2))
+f = open('keywords.txt','r')
+f2 = open('sentences.txt','r')
+keywords_list = f2.read()
+print(json.dumps(alchemy_language.entities(text=keywords_list), indent=2))
+print(json.dumps(alchemy_language.keywords(text=keywords_list), indent=2))
 # print(json.dumps(alchemy_language.category(url=url), indent=2))
 # print(json.dumps(alchemy_language.typed_relations(url=url), indent=2))
 # print(json.dumps(alchemy_language.relations(url=url), indent=2))
@@ -28,8 +26,8 @@ print(json.dumps(alchemy_language.targeted_sentiment(text='I love cats! Dogs are
 # print(json.dumps(alchemy_language.microformats(url='http://microformats.org/wiki/hcard-examples'), indent=2))
 # print(json.dumps(alchemy_language.publication_date(url=url), indent=2))
 # print(json.dumps(alchemy_language.taxonomy(url=url), indent=2))
-combined_operations = ['page-image', 'entity', 'keyword', 'title', 'author', 'taxonomy', 'concept', 'doc-emotion']
-print(json.dumps(alchemy_language.combined(url=url, extract=combined_operations), indent=2))
+# combined_operations = ['page-image', 'entity', 'keyword', 'title', 'author', 'taxonomy', 'concept', 'doc-emotion']
+# print(json.dumps(alchemy_language.combined(url=url, extract=combined_operations), indent=2))
 
 # Get sentiment and emotion information results for detected entities/keywords:
 # print(json.dumps(alchemy_language.entities(url=url, sentiment=True, emotion=True), indent=2))
